f2956f665f74bdd4469ca6fc05e9921819aa5e61
Max Mitchell <maxemitchell@gmail.com>
2025-07-13 13:56:12 -0500

    Change to Supabase SSR

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,20 @@
 <script lang="ts">
 	import '../app.css';
+	import { invalidate } from '$app/navigation';
+	import { onMount } from 'svelte';
 
-	let { children } = $props();
+	let { data, children } = $props();
+	let { session, supabase } = $derived(data);
+
+	onMount(() => {
+		const { data } = supabase.auth.onAuthStateChange((_, newSession) => {
+			if (newSession?.expires_at !== session?.expires_at) {
+				invalidate('supabase:auth');
+			}
+		});
+
+		return () => data.subscription.unsubscribe();
+	});
 </script>
 
 {@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,11 @@
 <script>
 	let { data } = $props();
+	let { test_table } = $derived(data);
 </script>
 
+<h1>Welcome to Supabase!</h1>
 <ul>
-	{#each data.test_table as test_table (test_table.id)}
+	{#each test_table as test_table (test_table.id)}
 		<li>{test_table.something}</li>
 	{/each}
 </ul>
